fe8073c7229e34aed947b4b0108ac9a41209a589
Efecan Bahcivanoglu <52242568+EfecanBahcivanoglu@users.noreply.github.com>
2020-08-07 11:42:21 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+//Import routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+//Routes
+app.get('/', (req,res) => {
+    res.send('We are on home')
+
+
+});
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },() => 
+    console.log('Connected to DB')
+);
+
+app.listen(2020);
